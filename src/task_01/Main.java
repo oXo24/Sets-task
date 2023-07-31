4d7f0b579b4d02935c60dbfc43ecef07e8d0699a
pavly <pavlykas0124@gmail.com>
2023-07-31 18:57:41 +0300

    Competed 2 whole tasks

--- src/task_01/Main.java
+++ src/task_01/Main.java
@@ -1,4 +1,50 @@
 package task_01;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Initial data:");
+        Set<String> dataSet = initialData();
+        displayData(dataSet);
+
+        System.out.println("\nUpdated data:");
+        updateData(dataSet, newData());
+        displayData(dataSet);
+    }
+
+    private static Set<String> initialData() {
+
+        Set<String> dataSet = new HashSet<>();
+
+        dataSet.add("orange");
+        dataSet.add("apple");
+        dataSet.add("kiwi");
+        dataSet.add("strawberry");
+        dataSet.add("mango");
+
+        return dataSet;
+    }
+
+    private static String newData() {
+
+        return "grape";
+    }
+
+    private static void updateData(Set<String> dataSet, String newData) {
+
+        dataSet.add(newData);
+    }
+
+    private static void displayData(Set<String> dataSet) {
+
+        int i = 1;
+        for (String data : dataSet) {
+            System.out.println(i + ") " + data);
+            i++;
+        }
+    }
 }
--- src/task_02/Main.java
+++ src/task_02/Main.java
@@ -1,4 +1,50 @@
 package task_02;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Initial data:");
+        Set<String> dataSet = initialData();
+        displayData(dataSet);
+
+        System.out.println("\nUpdated data:");
+        removeData(dataSet, dataToRemove());
+        displayData(dataSet);
+    }
+
+    private static Set<String> initialData() {
+
+        Set<String> dataSet = new HashSet<>();
+
+        dataSet.add("orange");
+        dataSet.add("apple");
+        dataSet.add("kiwi");
+        dataSet.add("strawberry");
+        dataSet.add("mango");
+
+        return dataSet;
+    }
+
+    private static String dataToRemove() {
+
+        return "kiwi";
+    }
+
+    private static void removeData(Set<String> dataSet, String dataToRemove) {
+
+        dataSet.remove(dataToRemove);
+    }
+
+    private static void displayData(Set<String> dataSet) {
+
+        int i = 1;
+        for (String data : dataSet) {
+            System.out.println(i + ") " + data);
+            i++;
+        }
+    }
 }
